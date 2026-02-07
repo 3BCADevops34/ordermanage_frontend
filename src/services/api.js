a8dfe4a705ac16476cc24ca2d90b092a2384809d
lechu <lechusubi6@gmail.com>
2026-02-07 20:31:05 +0530

    updated backend url1

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL + '/api';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL + '/api',
 });
 
 // Product API
